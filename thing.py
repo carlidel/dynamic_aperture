432f2c6f5b87caaa5f3d52d91d30e9e96c9886c7
Carlo Emilio Montanari <carlidel@ymail.com>
2018-06-29 16:46:16 +0200

    first big test

--- thing.py
+++ thing.py
@@ -1,8 +1,31 @@
 from tracker import *
 import matplotlib.pyplot as plt
 
-look = modulated_map_scan(0,1,0,1,200,10,1000,1)
+# "The step in r must be equal to the step
+# in theta times <|dr/dtheta|> to optimize
+# the integration steps."
+
+# TODO :: elaborate new method of stability map display
+
+'''
+look = modulated_map_scan(-1,1,0,1,200,100,1000,1)
 look[look != -1] = 0
 look[look == -1] = 1
-plt.imshow(look, origin='lower', extent = (-1, 1, 0, 1))
+print(look)
+plt.imshow(look, origin='lower', extent = (0, 1, -1, 1))
 plt.show()
+'''
+dx = 0.01
+nangles = 30
+angles = np.linspace(0, np.pi/4, nangles)
+epsilons = [0, 1, 2, 4, 16, 32, 64]
+n_turns = [1000, 10000, 100000, 1000000, 10000000]
+
+for t in n_turns:
+	for e in epsilons:
+		print("Working now on t={}, epsilons={}...".format(t,e))
+		data = np.empty((len(angles)))
+		for i in range(len(angles)):
+			data[i] = modulated_radius_scan(angles[i], dx, t, e)
+		np.save("radscan_T{}_dx{}_nthet{}_epsilon{}".format(t,dx,nangles,e), data)
+
