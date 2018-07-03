ebc35f2cc86b9f522659bc4e70f1240bea0acbaa
Carlo Emilio Montanari <carlidel@ymail.com>
2018-07-03 12:23:52 +0200

    c++ code and cluster job initialization

--- fit.py
+++ fit.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Load Data
+# Initialize Parameters used in the simulation
 
 dx = 0.01
 nangles = 30
@@ -15,6 +15,8 @@ n_turns = [1000, 2000, 5000, 7000, 10000, 20000, 50000, 100000, 1000000]
 
 n_turns = np.asarray(n_turns)
 
+# Load data
+
 data = []
 
 for i in range(len(epsilons)):
@@ -24,7 +26,7 @@ for i in range(len(epsilons)):
 		temp.append(scan)
 	data.append(temp)
 
-# Compute D and Error
+# Compute D and Error estimation of D
 
 D = []
 Err = []
@@ -34,8 +36,10 @@ for i in range(len(epsilons)):
 	temp_err = []
 	for j in range(len(n_turns)):
 		print(data[i][j])
-		# Basic average for now...
+		
+		# Basic average for D now...
 		temp.append(np.average(data[i][j]))
+		
 		# Error estimation...
 		first_derivatives = np.asarray([(data[i][j][k+1]-data[i][j][k]) / dtheta for k in range(len(data[i][j]) - 1)])
 		print(first_derivatives)
@@ -48,32 +52,46 @@ for i in range(len(epsilons)):
 D = np.asarray(D)
 Err = np.asarray(Err)
 
+# How's the data?
+
 np.set_printoptions(precision=3)
 
 print(D)
 print(Err)
 
-# Start non linear Fit
+# Non linear Fit
 
 from scipy.optimize import curve_fit
 
+# The k we are going to explore
 constant = 1
+
+# The function on which we execute the fit
 def func(x, A, B):
 	return A + B / np.log10(x)**constant
 
+# The goal is to minimize this
 def chi_squared(x, y, sigma, popt, k):
 	return (1 / (len(n_turns)-3)) * np.sum(((y - popt[0] - popt[1] / np.log10(x)**k) / sigma)**2)
 
+# For every Epsilon measured...
+
 for i in range(len(D)):
+	# Explore different values of k in [-20,20]
 	explore_k = []
 	for number in np.linspace(-20,20,100):
 		constant = number
 		popt, pcov = curve_fit(func, n_turns, D[i], sigma = Err[i])
 		explore_k.append(chi_squared(n_turns, D[i], Err[i], popt, constant))
+	
+	# Select Best k and re-execute fit
 	explore_k = np.asarray(explore_k)
 	print(explore_k.min(), np.linspace(-20,20,100)[explore_k.argmin()])
+	
 	constant = np.linspace(-20,20,100)[explore_k.argmin()]
 	popt, pcov = curve_fit(func, n_turns, D[i], sigma = Err[i])
+	
+	# Plot everything
 	plt.errorbar(n_turns, D[i], yerr = Err[i], linewidth = 0, elinewidth = 2, label = 'Data')
 	plt.plot(n_turns, func(n_turns, popt[0], popt[1]), 'g--', label = 'fit: A={:6.3f}, B={:6.3f}, k={:6.3f}'.format(popt[0], popt[1], constant))
 	plt.axhline(y = popt[0], color = 'r', linestyle = '-', label = 'y=A={:6.3f}'.format(popt[0]))
--- grid.cpp
+++ grid.cpp
@@ -1,37 +1,138 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
 #define _USE_MATH_DEFINES
 #include <cmath>
+#include <string>
 
 // Variables
 
-int iterations = 1000;
-int side = 100;
-double lenght = 0.6;
-double mu = -0.2;
-double ni_x = 0.25;
-double ni_y = 0.61803;
-double boundary = 10.0;
-
-double henon_map(double x, double px, double y, double py)
+double epsilon_k[7] =
 {
-	double omega_x = ni_x * 2 * M_PI;
-	double omega_y = ni_y * 2 * M_PI;
+	1.000e-4,
+	0.218e-4, 
+	0.708e-4, 
+	0.254e-4, 
+	0.100e-4, 
+	0.078e-4, 
+	0.218e-4
+};
+
+double Omega_k[7] =
+{
+	1  *(2 * M_PI / 868.12),
+	2  *(2 * M_PI / 868.12),
+	3  *(2 * M_PI / 868.12),
+	6  *(2 * M_PI / 868.12),
+	7  *(2 * M_PI / 868.12),
+	10 *(2 * M_PI / 868.12),
+	12 *(2 * M_PI / 868.12),
+};
+
+double omega_x0 = 0.168 * 2 * M_PI;
+double omega_y0 = 0.201 * 2 * M_PI;
+
+double * modulated_hennon_map(double * v0, double epsilon, unsigned int n)
+{
+	double sum = 0;
+	for (int i = 0; i < 7; ++i)
+		sum += epsilon_k[i] * cos(Omega_k[i] * n);
+	double omega_x = omega_x0 * (1 + epsilon * sum);
+	double omega_y = omega_y0 * (1 + epsilon * sum);
 
 	double cosx = cos(omega_x);
 	double sinx = sin(omega_x);
 	double cosy = cos(omega_y);
 	double siny = sin(omega_y);
 
-	double L[4][4] = 
+	double v[4];
+	// orario o antiorario?
+	v[0] = cosx * v0[0] + sinx * (v0[1] + v0[0] * v0[0] - v0[2] * v0[2]);
+	v[1] = -sinx * v0[0] + cosx * (v0[1] + v0[0] * v0[0] - v0[2] * v0[2]);
+	v[2] = cosy * v0[2] + siny * (v0[3] - 2 * v0[0] * v0[2]);
+	v[3] = -siny * v0[2] + cosy * (v0[3] - 2 * v0[0] * v0[2]);
+
+	v0[0] = v[0];
+	v0[1] = v[1];
+	v0[2] = v[2];
+	v0[3] = v[3];
+
+	return v0;
+}
+
+int modulated_particle(double x0, double y0, unsigned int T, double epsilon)
+{
+	double * v = new double [4];
+	v[0] = x0;
+	v[1] = 0;
+	v[2] = y0;
+	v[3] = 0;
+
+	for (unsigned int i = 0; i < T; ++i)
 	{
-		cosx, sinx, 0, 0,
-		-sinx, cosx,0, 0,
-		0, 0, cosy, siny,
-		0, 0, -siny, cosy
-	};
-	double v[4] = 
+		v = modulated_hennon_map(v, epsilon, i);
+		if (v[0]*v[0] + v[2]*v[2] > 1000000)
+		{
+			// Particle lost!
+			delete v;
+			return i;
+		}
+	}
+	// Particle not lost!
+	delete v;
+	return -1;
+}
+
+std::vector<double> * modulated_radius_scan(double theta, double dx, double epsilon, unsigned int max_turns = 10000000, unsigned int min_turns = 1000)
+{
+	std::vector<double> * v = new std::vector<double>();
+	
+	unsigned int actual_turns = max_turns;
+	int temp;
+	int i = -1;
+	
+	while(actual_turns >= min_turns)
 	{
-		x,
-		px + x*x - y*y + mu*()
-	};
+		i++;
+		temp = modulated_particle(i * dx * cos(theta), i * dx * sin(theta), actual_turns, epsilon);
+		if (temp != -1)
+		{
+			actual_turns = temp;
+			v->push_back(actual_turns);
+		}
+		else
+		{
+			v->push_back(actual_turns);
+		}
+	}
+	return v;
+}
+
+int main(int argc, const char * argv[])
+{
+	double dx = atof(argv[1]);
+	double n_theta = atoi(argv[2]);
+	double epsilon = atof(argv[3]);
+	
+	double d_theta = M_PI / (4 * n_theta);
+
+	std::string filename = "radscan_dx" + std::to_string(dx) + "_nthet" + std::to_string(n_theta) + "_epsilon" + std::to_string(epsilon) + ".txt";
+
+	std::ofstream out (filename, std::ofstream::out);
+
+	for (double angle = 0; angle <= M_PI / 4; angle += d_theta)
+	{
+		std::cout << "Scanning angle: " << angle << "/" << M_PI / 4 << std::endl;
+		out << angle << " ";
+		std::vector<double> * v = modulated_radius_scan(angle, dx, epsilon);
+		for (unsigned int i = 0; i < v->size(); ++i)
+		{
+			//std::cout << v->at(i) << " ";
+			out << v->at(i) << " ";
+		}
+		//std::cout << std::endl;
+		out << std::endl;
+		delete v;
+	}
+	return 0;
 }
